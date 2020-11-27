4d7bb6a94262d6de1c62d1a30d1ccaf144eb00c8
Victor Escoto (Gmail) <victor.e.magalhaes@gmail.com>
2020-11-26 21:36:47 -0300

    Adds AppBar component and its tests

---
+++ src/components/AppBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="mb-12 p-6 shadow-custom-header bg-white text-center">
+    <span class="text-primary text-3xl">SWStarter</span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+};
+</script>
